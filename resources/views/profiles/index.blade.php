4004f0cc23ebbcdeb550043b4e4c132b776be5b8
huongvu2312 <calm.and.chaotic@gmail.com>
2020-02-18 23:56:19 +0100

    [UI] Make post order from earliest timestamp, and count total amount of posts

--- resources/views/profiles/index.blade.php
+++ resources/views/profiles/index.blade.php
@@ -9,10 +9,10 @@
         <div class="col-9 p-5">
             <div class="d-flex justify-content-between align-items-baseline">
                 <h1>{{ $user->username }}</h1>
-                <a href="#">Add new post</a>
+                <a href="/p/create">Add new post</a>
             </div>
             <div class="d-flex">
-                <div class="pr-5"><strong>153</strong> posts</div>
+                <div class="pr-5"><strong>{{ $user->posts->count() }}</strong> posts</div>
                 <div class="pr-5"><strong>23k</strong> followers</div>
                 <div class="pr-5"><strong>212</strong> following</div>
             </div>
@@ -20,18 +20,14 @@
             <div>{{ $user->profile->description }}</div>
             <div><a href="#">{{ $user->profile->url }}</a></div>
         </div>
+    </div>
 
-        <div class="row pt-5">
-            <div class="col-4">
-                <img src="/pic/rido.jpg" class="w-100">
-            </div>
-            <div class="col-4">
-                <img src="/pic/rido.jpg" class="w-100">
-            </div>
-            <div class="col-4">
-                <img src="/pic/rido.jpg" class="w-100">
-            </div>
+    <div class="row pt-5">
+        @foreach($user->posts as $post)
+        <div class="col-4 pb-4">
+            <img src="/storage/{{ $post->image }}" class="w-100">
         </div>
+        @endforeach
     </div>
 </div>
 @endsection
