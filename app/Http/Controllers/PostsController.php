53e03fbd02ffabf963960aaa43a362ecfc713cf5
huongvu2312 <calm.and.chaotic@gmail.com>
2020-02-19 00:09:41 +0100

    [UI] Set image in post to be concrete size

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Intervention\Image\Facades\Image;
 
 class PostsController extends Controller
 {
@@ -26,6 +27,9 @@ class PostsController extends Controller
         ]);
 
         $imagePath = request('image')->store('uploads', 'public');
+
+        $image = Image::make(public_path("storage/{$imagePath}"))->fit(1200, 1200);
+        $image->save();
 
         // Grab authentication user, go to their post and create
         auth()->user()->posts()->create([
