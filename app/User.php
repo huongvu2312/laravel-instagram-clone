31c261a63c2c2204a4f944688ecf9f12fda15763
huongvu2312 <calm.and.chaotic@gmail.com>
2020-02-18 22:44:15 +0100

    [Model] Set post model and add-new-post button

--- app/User.php
+++ app/User.php
@@ -36,4 +36,14 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function profile()
+    {
+        return $this->hasOne(Profile::class);
+    }
+
+    public function posts()
+    {
+        return $this->hasMany(Post::class);
+    }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,15 +7,18 @@
             <img src="/pic/monokuma.jpg" class="rounded-circle">
         </div>
         <div class="col-9 p-5">
-            <div><h1>{{ $user->username }}</h1></div>
+            <div class="d-flex justify-content-between align-items-baseline">
+                <h1>{{ $user->username }}</h1>
+                <a href="#">Add new post</a>
+            </div>
             <div class="d-flex">
                 <div class="pr-5"><strong>153</strong> posts</div>
                 <div class="pr-5"><strong>23k</strong> followers</div>
                 <div class="pr-5"><strong>212</strong> following</div>
             </div>
-            <div class="pt-4 font-weight-bold">thvu.net</div>
-            <div>I am a young web developer who love to tingle with all kind of magic- I mean technologies.</div>
-            <div><a href="#">www.thvu.net</a></div>
+            <div class="pt-4 font-weight-bold">{{ $user->profile->title }}</div>
+            <div>{{ $user->profile->description }}</div>
+            <div><a href="#">{{ $user->profile->url }}</a></div>
         </div>
 
         <div class="row pt-5">
@@ -26,7 +29,7 @@
                 <img src="/pic/rido.jpg" class="w-100">
             </div>
             <div class="col-4">
-                <img src="/pic/rido.jpg" class="w-100" >
+                <img src="/pic/rido.jpg" class="w-100">
             </div>
         </div>
     </div>
