8778b399cc562fb2a8d8d62d3aba282bc2a4268b
huongvu2312 <calm.and.chaotic@gmail.com>
2020-02-18 23:11:09 +0100

    [UI] Finish UI for add-new-post page

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -2,12 +2,16 @@
 
 @section('content')
 <div class="container">
-    <div class="row">
-        <div class="col-8 offset-2">
-            <div class="form-group row">
-                <label for="caption" class="col-md-4 col-form-label text-md-right">Post Caption</label>
+    <form action="/p" enctype="multipart/form-data" method="post">
+        <div class="row">
+            <div class="col-8 offset-2">
+                <div class="row">
+                    <h1>Add New Post</h1>
+                </div>
 
-                <div class="col-md-6">
+                <div class="form-group row">
+                    <label for="caption" class="col-md-4 col-form-label">Post Caption</label>
+
                     <input id="caption" type="text" class="form-control @error('caption') is-invalid @enderror"
                         name="caption" value="{{ old('caption') }}" required autocomplete="caption" autofocus>
 
@@ -17,8 +21,24 @@
                     </span>
                     @enderror
                 </div>
+
+                <div class="row">
+                    <label for="image" class="col-md-4 col-form-label">Post Image</label>
+                    <input type="file" class="form-control-file" id="image" name="image">
+
+                    @error('image')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                    @enderror
+                </div>
+
+                <div class="row pt-4">
+                    <button class="btn btn-primary">Add New Post</button>
+                </div>
+
             </div>
         </div>
-    </div>
+    </form>
 </div>
 @endsection
