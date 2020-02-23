a9bc9d09b892fccc2c2930979a2adaf82c3e156a
huongvu2312 <calm.and.chaotic@gmail.com>
2020-02-23 20:06:00 +0100

    [FUNCTION] Add default avatar when new user is created

--- app/Profile.php
+++ app/Profile.php
@@ -7,6 +7,12 @@ use Illuminate\Database\Eloquent\Model;
 class Profile extends Model
 {
     protected $guarded = [];
+
+    public function profileImage()
+    {
+        $imagePath = ($this->image) ? $this->image : 'profile/uv7DE6VAFjoRu5IGeivRyXTG8mrxxTgKiABPTghu.png';
+        return '/storage/' . $imagePath;
+    }
     public function user()
     {
         return $this->belongsTo(User::class);
