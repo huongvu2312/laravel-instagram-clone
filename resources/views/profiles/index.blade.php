3a3be95ef5a36b3e751707acebba3d93f40b80f7
huongvu2312 <calm.and.chaotic@gmail.com>
2020-02-23 20:11:26 +0100

    [UI] Add follow button to profile

--- resources/views/profiles/index.blade.php
+++ resources/views/profiles/index.blade.php
@@ -8,7 +8,10 @@
         </div>
         <div class="col-9 p-5">
             <div class="d-flex justify-content-between align-items-baseline">
-                <h1>{{ $user->username }}</h1>
+                <div class="d-flex align-items-center pb-3">
+                    <div class="h4">{{ $user->username }}</div>
+                    <button class="btn btn-primary ml-4">Follow</button>
+                </div>
                 @can('update', $user->profile)
                 <a href="/p/create">Add new post</a>
                 @endcan
